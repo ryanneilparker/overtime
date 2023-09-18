70e4875fc17dd794e145976ea5021edea040a996
= <ryanneilparker@gmail.com>
2023-09-18 18:14:19 +0200

    Add navbar + footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,28 @@
+<footer class="bg-light">
+	<div class="container">
+		<div class="row justify-content-space-around">
+			<div class="col-8">
+				<p>
+					<a href="https://github.com/ryanneilparker/overtime.git"
+						><strong>Overtime Tracker</strong></a
+					>
+				</p>
+				<p>
+					Made with ❤️ by
+					<a href="https://github.com/ryanneilparker">ryanneilparker</a>.
+				</p>
+			</div>
+			<div class="col-4">
+				<p><strong>Allan Gray Investments</strong></p>
+				<p>Created September 18th, 2023.</p>
+			</div>
+		</div>
+		<hr />
+	</div>
+</footer>
+
+<style>
+	footer {
+		margin-top: auto;
+	}
+</style>
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/">Overtime Tracker</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNavAltMarkup"
+			aria-controls="navbarNavAltMarkup"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-link" href="/">Overview</a>
+				<a class="nav-link" href="/ticket/create">Create Ticket</a>
+			</div>
+		</div>
+	</div>
+</nav>
